ddef2d4be9681c6b0c3525fe6bcfa443e2a8cf10
Tramort <jake@gogo.by>
2014-05-17 20:04:54 +0300

    Added tracbuildbot/tools.py with Singleton superclass.

---
+++ tracbuildbot/tools.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Max Kaskevich <maxim.kaskevich@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+class SingletonMeta(type):
+    def __init__(cls, name, bases, dict):
+        super(SingletonMeta, cls).__init__(name, bases, dict)
+        cls.instance = None
+    def __call__(self,*args,**kw):
+        if self.instance is None:
+            self.instance = super(SingletonMeta, self).__call__(*args, **kw)
+        return self.instance
+ 
+class Singleton(object):
+    __metaclass__ = SingletonMeta
+
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
